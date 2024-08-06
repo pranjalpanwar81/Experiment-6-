63764fc98bfc3c0525550afe5aab117863bbdcce
Pranjal Panwar <138666385+pranjalpanwar81@users.noreply.github.com>
2024-08-07 02:17:38 +0530

    Create Exp6(C).cpp

---
+++ Exp6(C).cpp
@@ -0,0 +1,17 @@
+// NAME - Pranjal Panwar
+// PRN - 23070123164 
+// EXPERIMENT -6(C) 
+
+#include<iostream> 
+using namespace std;
+
+int main() 
+{
+    int a=0;
+    do {
+        cout<< a << "\n";
+        a=a+2;
+    }
+    while (a<=50); 
+    return 0; 
+}
