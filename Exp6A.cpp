22a0f69b530feb10b42640db1152b4e4cb462ae9
Pranjal Panwar <138666385+pranjalpanwar81@users.noreply.github.com>
2024-08-07 02:14:50 +0530

    Create Exp6A.cpp

---
+++ Exp6A.cpp
@@ -0,0 +1,13 @@
+// NAME - Pranjal Panwar 
+// PRN -2307023164
+// EXPERIMENT -6(A) 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    for (i=0;i<10;i++) {
+    cout<<"HELLO WORLD("<<i+1<<")"<<"\n";
+    }
+    return 0;
+}
